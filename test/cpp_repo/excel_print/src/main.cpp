8d0df338c776a7e91bb41a59c735109c7163aba5
Xylow <gabriel_a_galvao@hotmail.com>
2023-03-01 23:12:59 +0100

    Add main.cpp test file, rework Excel test file

---
+++ test/cpp_repo/excel_print/src/main.cpp
@@ -0,0 +1,39 @@
+/**
+ * @file main.cpp
+ * @author Gabriel Galvao
+ * @brief Main source file for testing auto-coding
+ * @version 0.1
+ * @date 2023-03-01
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+
+#include <Arduino.h>
+
+// Global variables
+float param1, param2, param3, param4, param5, param6;
+
+// Set initial variable values
+void setup()
+{
+    param1 = 1.0;
+    param2 = 2.0;
+    param3 = 3.0;
+    param4 = 4.0;
+    param5 = 5.0;
+    param6 = 6.0;
+}
+
+// Implement equations
+void loop()
+{
+    // -###- Code automation
+    param1 = 0; 
+    param2 = 0;
+    param3 = 0;
+    param4 = 0;
+    param5 = 0;
+    param6 = 0;
+}
